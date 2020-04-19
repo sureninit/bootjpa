42565f002c518754fd6343dd6acd2f5fbfc865f3
sthsurendra <38010470+sthsurendra@users.noreply.github.com>
2020-04-18 23:12:22 -0600

    add comment

--- src/main/java/com/bootJpa/model/AlienController.java
+++ src/main/java/com/bootJpa/model/AlienController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bootJpa.dao.AlienRepo;
-
+//fine programee
 //@RestController // restapi banaunako lagi
 @Controller// servlet banaunako lagi
 public class AlienController {
